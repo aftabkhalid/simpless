33411249e14632f715b70f3a3659732bdaba63d9
aftabkhalid <iffi.d.s@gmail.com>
2013-01-08 21:23:24 +0500

    added wp files

---
+++ footer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content after
+ *
+ * @package simpless
+ * @since simpless 1.0
+ */
+?>
+
+	</div><!-- #main .site-main -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<?php do_action( 'simpless_credits' ); ?>
+			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'simpless' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'simpless' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'simpless' ), 'simpless', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon .site-footer -->
+</div><!-- #page .hfeed .site -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ inc/customizer.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * simpless Theme Customizer
+ *
+ * @package simpless
+ * @since simpless 1.2
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ *
+ * @since simpless 1.2
+ */
+function simpless_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport        = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'simpless_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ *
+ * @since simpless 1.2
+ */
+function simpless_customize_preview_js() {
+	wp_enqueue_script( 'simpless_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20120827', true );
+}
+add_action( 'customize_preview_init', 'simpless_customize_preview_js' );
